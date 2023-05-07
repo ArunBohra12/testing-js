ea1ac0063f19c99a16b611fec2192785fee9a67d
Arun Bohra <arunbohra12@gmail.com>
2023-05-07 09:20:24 +0530

    Learn matchers in jest

--- jest/__test__/index.test.js
+++ jest/__test__/index.test.js
@@ -1,5 +1,74 @@
-const { add } = require("..");
+const { add, fibonacci } = require("..");
 
 test("adds 1 + 2 to equal 3", function () {
   expect(add(1, 2)).toBe(3);
+});
+
+describe("Fibonacci sequence", function () {
+  test("Input - 3", function () {
+    expect(fibonacci(3)).toBe(2);
+  });
+
+  test("Input - 10", function () {
+    expect(fibonacci(10)).toBe(55);
+  });
+
+  test("Input - 20", function () {
+    expect(fibonacci(20)).toBe(6765);
+  });
+});
+
+describe("Testing object values", function () {
+  const userData = {
+    name: "Arun Bohra",
+    age: 19,
+  };
+
+  test("toEquals method", function () {
+    expect(userData).toEqual({ name: "Arun Bohra", age: 19 });
+  });
+});
+
+test("NOT check: Sum of positive nums can't be 0", function () {
+  for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+      expect(i + j).not.toBe(0);
+    }
+  }
+});
+
+describe("Test for truthiness", function () {
+  test("Null check", function () {
+    const n = null;
+
+    expect(n).toBeNull();
+    // expect(n).toBeDefined();
+    // expect(n).not.toBeDefined();
+    // expect(n).toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+
+  test("Zero check", function () {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+  });
+});
+
+const shoppingList = [
+  "diapers",
+  "kleenex",
+  "trash bags",
+  "paper towels",
+  "milk",
+];
+
+describe("Arrays and iterables", function () {
+  test("Arrays and sets", function () {
+    expect(shoppingList).tocontain("paper towels");
+    expect(new Set(shoppingList)).toContain("kleenex");
+  });
 });
--- jest/index.js
+++ jest/index.js
@@ -1,3 +1,17 @@
 exports.add = function (a, b) {
   return a + b;
 };
+
+exports.fibonacci = (n) => {
+  let a = 0,
+    b = 1,
+    c = n;
+
+  for (let i = 2; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  return c;
+};
